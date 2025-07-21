a45c6ab7926080e4add18350ed2faba810260c22
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2025-07-21 07:25:18 +0300

    added terraform code

---
+++ infra/ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "webserver" {
+  for_each = {for i in range(2) :"instance${i}" => i}
+  ami                    = "ami-09c6c60df90f53603"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.main.id]
+  tags = {
+    "Name" = "webserver${each.value}"
+  }
+  key_name = "jenkins-key"
+}
---
+++ infra/security_group.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "main" {
+
+}
+
+resource "aws_security_group_rule" "allow_http" {
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  type              = "ingress"
+  security_group_id = aws_security_group.main.id
+}
+
+resource "aws_security_group_rule" "allow_ssh" {
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  type              = "ingress"
+  security_group_id = aws_security_group.main.id
+}
+
+resource "aws_security_group_rule" "allow_egress" {
+  from_port         = 0
+  to_port           = 0
+  protocol          = "all"
+  cidr_blocks       = ["0.0.0.0/0"]
+  type              = "egress"
+  security_group_id = aws_security_group.main.id
+}
