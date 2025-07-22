a0a9d5d685432ebe47f898425142fb5f518c1d0d
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2025-07-22 21:46:52 +0300

    updated inventory file

--- ansible/inventory.py
+++ ansible/inventory.py
@@ -1,12 +1,41 @@
 #!/usr/bin/env python3
 import json
+import sys
 
 with open("ansible/ips.json") as f:
     hosts = json.load(f)
 
-hosts_dict = {"all": {"hosts": {}, "children": {"webservers": {"hosts": {}}}}}
+hosts_dict = {
+        "_meta": 
+        {
+            "hostvars":
+            {
+
+            }    
+        }, 
+        "all": 
+        {
+           "children": 
+            [
+                "webservers"
+            ]
+        },
+        "webservers": 
+        {
+            "hosts":
+            [
+
+            ]
+        }
+        
+}
 
 for name,ip in hosts["ips"]["value"].items():
-    hosts_dict["all"]["hosts"][name] = {"ansible_host": ip}
-    hosts_dict["all"]["children"]["webservers"]["hosts"][name] = None
-print(json.dumps(hosts_dict, indent=2))
+    hosts_dict["_meta"]["hostvars"][name] = {"ansible_host": ip}
+    hosts_dict["webservers"]["hosts"].append(name)
+
+
+if len(sys.argv) == 2 and sys.argv[1] == "--list":
+    print(json.dumps(hosts_dict, indent=2))
+elif len(sys.argv) == 3 and sys.argv[1] == "--host":
+    print(json.dumps(hosts_dict["all"]["webservers"][sys.argv[2]]), indent=2)
