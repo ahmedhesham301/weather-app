7e801c7000db209cbd78c5203b0d167f8a25671f
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2025-07-22 16:05:20 +0300

    updated inventory.py

--- ansible/inventory.py
+++ ansible/inventory.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 import json
+import sys
 
 with open("ansible/ips.json") as f:
-    hosts = json.load(f)
+    ip_data = json.load(f)
 
-hosts_dict = {"all": {"hosts": {}, "children": {"webservers": {"hosts": {}}}}}
+hosts = ip_data["ips"]["value"]
 
-for name,ip in hosts["ips"]["value"].items():
-    hosts_dict["all"]["hosts"][name] = {"ansible_host": ip}
-    hosts_dict["all"]["children"]["webservers"]["hosts"][name] = None
-print(json.dumps(hosts_dict, indent=2))
+inventory = {
+    "all": {"children": {"webservers": {"hosts": list(hosts.keys())}}},
+    "_meta": {"hostvars": {name: {"ansible_host": ip} for name, ip in hosts.items()}},
+}
+
+if len(sys.argv) == 2 and sys.argv[1] == "--list":
+    print(json.dumps(inventory, indent=2))
+elif len(sys.argv) == 3 and sys.argv[1] == "--host":
+    hostname = sys.argv[2]
+    print(json.dumps(inventory["_meta"]["hostvars"].get(hostname, {}), indent=2))
+else:
+    print("Usage: --list or --host <hostname>", file=sys.stderr)
+    sys.exit(1)
