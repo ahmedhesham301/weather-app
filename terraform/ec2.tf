6abd4a58bf62b0ca3c1fbb420572c39349eb6db9
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2025-08-01 11:25:13 +0300

    updated it so that it creates a subnet in each az lanches each instances in a different subnet -> round robin

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,8 +1,12 @@
+locals {
+  azs = data.aws_availability_zones.available.names
+}
+
 resource "aws_instance" "webserver" {
   for_each               = { for i in range(var.instance_count) : "instance${i}" => i }
   ami                    = "ami-09c6c60df90f53603"
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.public.id
+  subnet_id              = aws_subnet.public[local.azs[each.value % length(local.azs)]].id
   vpc_security_group_ids = [aws_security_group.main.id]
   tags = {
     "Name" = "webserver${each.value}"
--- terraform/network.tf
+++ terraform/network.tf
@@ -1,5 +1,10 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    "Name" = "weather-app"
+  }
 }
 
 resource "aws_internet_gateway" "gw" {
@@ -7,9 +12,18 @@ resource "aws_internet_gateway" "gw" {
 }
 
 resource "aws_subnet" "public" {
+  for_each = {
+    for i, az in data.aws_availability_zones.available.names :
+    az => i
+  }
+
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, each.value)
+  availability_zone       = each.key
   map_public_ip_on_launch = true
+  tags = {
+    Name = "public-${each.key}"
+  }
 }
 
 resource "aws_route_table" "rt" {
@@ -18,10 +32,11 @@ resource "aws_route_table" "rt" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
-  
+
 }
 
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id
   route_table_id = aws_route_table.rt.id
 }
