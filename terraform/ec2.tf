459ae05cadbf126508fa12862453e4425fe1ab58
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2025-08-06 12:02:12 +0300

    updated the ami added userdata to the basation host to insatll ansible on startup

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "aws_instance" "webserver" {
   for_each               = { for i in range(var.instance_count) : "instance${i}" => i }
-  ami                    = "ami-09c6c60df90f53603"
+  ami                    = "ami-0c1b03e30bca3b373"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.private[local.azs[each.value % length(local.azs)]].id
   vpc_security_group_ids = [aws_security_group.webserver.id]
@@ -15,7 +15,7 @@ resource "aws_instance" "webserver" {
 }
 
 resource "aws_instance" "bastion" {
-  ami                    = "ami-09c6c60df90f53603"
+  ami                    = "ami-0c1b03e30bca3b373"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public[local.azs[0]].id
   vpc_security_group_ids = [aws_security_group.bastion.id]
@@ -23,5 +23,11 @@ resource "aws_instance" "bastion" {
     "Name" = "bastion"
   }
   key_name = "jenkins-key"
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum upgrade -y
+              yum install ansible -y
+              EOF
 
 }
